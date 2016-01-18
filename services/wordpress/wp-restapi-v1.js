6200f30f719adf7435ffc4b2025196ea9cb09a22
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2016-01-18 15:11:50 +0900

    Add license information

--- services/wordpress/wp-restapi-v1.js
+++ services/wordpress/wp-restapi-v1.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of ajaxbuilder.
+ * (c) 2015-2016 Tomoyuki Tsujimoto
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 "use strict";
 
 
